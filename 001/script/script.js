12326f09d08a30f84141d9bcb6cf606681085b43
m.osumi <m.osumi@goodpatch.com>
2020-05-03 21:31:46 +0900

    initial commit

---
+++ 001/script/script.js
@@ -0,0 +1,39 @@
+
+(function() {
+  /**
+   * canvasの幅
+   * @type {number}
+   */
+  const CANVAS_WIDTH = 640;
+
+  /**
+   * canvasの高さ
+   * @type {number}
+   */
+  const CANVAS_HEIGHT = 480;
+  let util;
+  let canvas;
+  let ctx;
+
+  window.addEventListener('load', () => {
+    util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
+    canvas = util.canvas;
+    ctx = util.context;
+
+    util.imageLoader('./image/viper.png', (loadedImage) => {
+      image = loadedImage;
+      initialize();
+      render();
+    });
+  });
+  function initialize() {
+    // canvasの大きさを設定
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+  }
+
+  function render() {
+    util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
+    ctx.drawImage(image, 100, 100);
+  }
+})();
