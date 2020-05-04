509f0095b22511da3dace110f4a2dca4e5f9b430
m.osumi <m.osumi@goodpatch.com>
2020-05-04 13:03:29 +0900

    feat: move viper by value of Math.sin

--- 001/script/script.js
+++ 001/script/script.js
@@ -14,6 +14,7 @@
   let util;
   let canvas;
   let ctx;
+  let startTime;
 
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -23,6 +24,7 @@
     util.imageLoader('./image/viper.png', (loadedImage) => {
       image = loadedImage;
       initialize();
+      startTime = Date.now();
       render();
     });
   });
@@ -34,6 +36,16 @@
 
   function render() {
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
-    ctx.drawImage(image, 100, 100);
+
+    // 現在までの経過時間を秒単位で取得する
+    let nowTime = (Date.now() - startTime) / 1000;
+    // 時間の経過が見た目でわかりやすいように自機をサイン波で動かす
+    let s = Math.sin(nowTime);
+    // サインやコサインは半径1の円を基準にしており、得られる範囲が-1.0~1.0になるため、効果がわかりやすいように100倍する
+    let x = s * 100.0;
+
+    ctx.drawImage(image, CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT / 2);
+
+    requestAnimationFrame(render);
   }
 })();
