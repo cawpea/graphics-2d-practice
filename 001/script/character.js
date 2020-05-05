9edf2baa48b81b9e1cf1659464ab0e8d016cdc49
m.osumi <m.osumi@goodpatch.com>
2020-05-05 17:30:56 +0900

    feat: add Shot for Viper

--- 001/script/character.js
+++ 001/script/character.js
@@ -22,15 +22,20 @@ class Character {
    * @param {number} x - X座標 
    * @param {number} y - Y座標 
    * @param {number} life - キャラクターのライフ(生存フラグを兼ねる)
-   * @param {Image} image - キャラクターの画像 
+   * @param {Image} imagePath - キャラクターの画像のパス
    */
-  constructor(ctx, x, y, w, h, life, image) {
+  constructor(ctx, x, y, w, h, life, imagePath) {
     this.ctx = ctx;
     this.position = new Position(x, y);
     this.width = w;
     this.height = h;
     this.life = life;
-    this.image = image;
+    this.ready = false;
+    this.image = new Image();
+    this.image.addEventListener('load', () => {
+      this.ready = true;
+    });
+    this.image.src = imagePath;
   }
   draw() {
     let offsetX = this.width / 2;
@@ -60,6 +65,7 @@ class Viper extends Character {
     this.comingStart = null;
     this.comingStartPosition = null;
     this.comingEndPosition = null;
+    this.shotArray = null;
   }
   setComing(startX, startY, endX, endY) {
     this.isComing = true;
@@ -67,6 +73,9 @@ class Viper extends Character {
     this.position.set(startX, startY);
     this.comingStartPosition = new Position(startX, startY);
     this.comingEndPosition = new Position(endX, endY);
+  }
+  setShotArray(shotArray) {
+    this.shotArray = shotArray;
   }
   update() {
     let justTime = Date.now();
@@ -101,9 +110,49 @@ class Viper extends Character {
       let tx = Math.min(Math.max(this.position.x, 0), canvasWidth);
       let ty = Math.min(Math.max(this.position.y, 0), canvasHeight);
       this.position.set(tx, ty);
+
+      if (window.isKeyDown.key_z) {
+        for (let i = 0; i < this.shotArray.length; i++) {
+          if (this.shotArray[i].life <= 0) {
+            this.shotArray[i].set(this.position.x, this.position.y);
+            // ひとつ生成したらループを抜ける
+            break;
+          }
+        }
+      }
     }
 
     this.draw();
     this.ctx.globalAlpha = 1.0;
+  }
+}
+
+class Shot extends Character {
+  /**
+   * @constructor
+   * @param {CanvasRenderingContext2D} ctx - 描画などに利用する 2D コンテキスト 
+   * @param {number} x - X座標
+   * @param {number} y - Y座標
+   * @param {number} w - 幅
+   * @param {number} h - 高さ
+   * @param {Image} imagePath - キャラクター用の画像パス
+   */
+  constructor(ctx, x, y, w, h, imagePath) {
+    super(ctx, x, y, w, h, 0, imagePath);
+    this.speed = 7;
+  }
+  set(x, y) {
+    this.position.set(x, y);
+    this.life = 1;
+  }
+  update() {
+    if (this.life <= 0) { return; }
+
+    if (this.position.y + this.height < 0) {
+      this.life = 0;
+    }
+    // ショットを上に向かって移動させる
+    this.position.y -= this.speed;
+    this.draw();
   }
 }
--- 001/script/script.js
+++ 001/script/script.js
@@ -14,6 +14,12 @@
   const CANVAS_WIDTH = 640;
 
   /**
+   * ショットの最大個数
+   * @type {number}
+   */
+  const SHOT_MAX_COUNT = 10;
+
+  /**
    * canvasの高さ
    * @type {number}
    */
@@ -23,19 +29,19 @@
   let ctx;
   let startTime;
   let viper = null;
+  /**
+   * ショットのインスタンスを格納する配列
+   * @type {Array<Shot>}
+   */
+  let shotArray = [];
 
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
     canvas = util.canvas;
     ctx = util.context;
 
-    util.imageLoader('./image/viper.png', (loadedImage) => {
-      image = loadedImage;
-      initialize();
-      eventSetting();
-      startTime = Date.now();
-      render();
-    });
+    initialize();
+    loadCheck();
   });
 
   function eventSetting() {
@@ -52,13 +58,35 @@
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
 
-    viper = new Viper(ctx, 0, 0, 64, 64, image);
+    viper = new Viper(ctx, 0, 0, 64, 64, './image/viper.png');
     viper.setComing(
       CANVAS_WIDTH / 2,
       CANVAS_HEIGHT,
       CANVAS_WIDTH / 2,
       CANVAS_HEIGHT - 100
-    )
+    );
+
+    for(let i = 0; i < SHOT_MAX_COUNT; i++) {
+      shotArray[i] = new Shot(ctx, 0, 0, 32, 32, './image/viper_shot.png');
+    }
+
+    viper.setShotArray(shotArray);
+  }
+
+  function loadCheck() {
+    let ready = true;
+    ready = ready && viper.ready;
+    shotArray.map(v => {
+      ready = ready && v.ready;
+    });
+
+    if (ready) {
+      eventSetting();
+      startTime = Date.now();
+      render();
+    } else {
+      setTimeout(loadCheck, 100);
+    }
   }
 
   function render() {
@@ -70,6 +98,11 @@
 
     // 自機キャラクターの状態を更新する
     viper.update();
+
+    // ショットの状態を更新する
+    shotArray.map((v) => {
+      v.update();
+    });
 
     requestAnimationFrame(render);
   }
