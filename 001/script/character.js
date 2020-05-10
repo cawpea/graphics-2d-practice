efa66295895cad4231a67a4eefbecb9ece485979
m.osumi <m.osumi@goodpatch.com>
2020-05-10 19:35:04 +0900

    :sparkels: appear enemy only one

--- 001/script/character.js
+++ 001/script/character.js
@@ -180,6 +180,30 @@ class Viper extends Character {
   }
 }
 
+class Enemy extends Character {
+  constructor(ctx, x, y, w, h, imagePath) {
+    super(ctx, x, y, w, h, 0, imagePath);
+    this.speed = 3;
+  }
+  set(x, y, life = 1) {
+    this.position.set(x, y);
+    this.life = life;
+  }
+  update() {
+    if (this.life <= 0) {
+      return;
+    }
+    // 敵キャラクターが画面外（画面下端）へ移動していたらライフを0に設定する
+    if (this.position.y - this.height > this.ctx.canvas.height) {
+      this.life = 0;
+    }
+    this.position.x += this.vector.x * this.speed;
+    this.position.y += this.vector.y * this.speed;
+
+    this.draw();
+  }
+}
+
 class Shot extends Character {
   /**
    * @constructor
--- 001/script/script.js
+++ 001/script/script.js
@@ -14,6 +14,11 @@
   const CANVAS_WIDTH = 640;
 
   /**
+   * 敵キャラクターのインスタンス数
+   */
+  const ENEMY_MAX_COUNT = 10;
+
+  /**
    * ショットの最大個数
    * @type {number}
    */
@@ -27,8 +32,15 @@
   let util;
   let canvas;
   let ctx;
+  let scene;
   let startTime;
   let viper = null;
+
+  /**
+   * 敵キャラクターのインスタンスを格納する配列
+   */
+  let enemyArray = [];
+
   /**
    * ショットのインスタンスを格納する配列
    * @type {Array<Shot>}
@@ -59,10 +71,34 @@
     }, false);
   }
 
+  function sceneSetting() {
+    scene.add('intro', (time) => {
+      if (time > 2.0) {
+        scene.use('invade');
+      }
+    });
+    scene.add('invade', (time) => {
+      if (scene.frame !== 0) {
+        return;
+      }
+      for(let i = 0; i < ENEMY_MAX_COUNT; ++i) {
+        if (enemyArray[i].life <= 0) {
+          let e = enemyArray[i];
+          e.set(CANVAS_WIDTH / 2, -e.height);
+          e.setVector(0.0, 1.0);
+          break;
+        }
+      }
+    });
+    scene.use('intro');
+  }
+
   function initialize() {
     // canvasの大きさを設定
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
+
+    scene = new SceneManager();
 
     viper = new Viper(ctx, 0, 0, 64, 64, './image/viper.png');
     viper.setComing(
@@ -71,6 +107,10 @@
       CANVAS_WIDTH / 2,
       CANVAS_HEIGHT - 100
     );
+
+    for(let i = 0; i < ENEMY_MAX_COUNT; i++) {
+      enemyArray[i] = new Enemy(ctx, 0, 0, 48, 48, './image/enemy_small.png');
+    }
 
     for(let i = 0; i < SHOT_MAX_COUNT; i++) {
       shotArray[i] = new Shot(ctx, 0, 0, 32, 32, './image/viper_shot.png');
@@ -84,6 +124,9 @@
   function loadCheck() {
     let ready = true;
     ready = ready && viper.ready;
+    enemyArray.map(v => {
+      ready = ready && v.ready;
+    });
     shotArray.map(v => {
       ready = ready && v.ready;
     });
@@ -93,6 +136,7 @@
 
     if (ready) {
       eventSetting();
+      sceneSetting();
       startTime = Date.now();
       render();
     } else {
@@ -107,8 +151,16 @@
     // 現在までの経過時間を秒単位で取得する
     let nowTime = (Date.now() - startTime) / 1000;
 
+    // シーンを更新する
+    scene.update();
+
     // 自機キャラクターの状態を更新する
     viper.update();
+
+    // 敵キャラクターの状態を更新する
+    enemyArray.map((v) => {
+      v.update();
+    });
 
     // ショットの状態を更新する
     shotArray.map((v) => {
